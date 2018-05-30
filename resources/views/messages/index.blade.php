f24af2203d358b1478b074e3a4f7a429e6179e0f
sara.kawamoto <sara.kawamoto@rakuten.com>
2018-05-30 01:18:01 +0000

    Procfile

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+s@extends('layouts.app')
 
 @section('content')
 
@@ -10,6 +10,26 @@
                 <li>{!! link_to_route('messages.show', $message->id, ['id' => $message->id]) !!} : {{ $message->title }} > {{ $message->content }}</li>
             @endforeach
         </ul>
+        @if (count($messages) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タイトル</th>
+                    <th>メッセージ</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($messages as $message)
+                    <tr>
+                        <td>{!! link_to_route('messages.show', $message->id, ['id' => $message->id]) !!}</td>
+                        <td>{{ $message->title }}</td>
+                        <td>{{ $message->content }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
     @endif
 
     {!! link_to_route('messages.create', '新規メッセージの投稿') !!}
